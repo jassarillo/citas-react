627318e42778d5e5fe25b7c1e59820fd13b14eaf
julio cesar arrieta santiago <jarrieta@finanzas.cdmx.gob.mx>
2022-05-11 17:58:57 -0500

    inicio configuracion

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+     <h1>Hola mundo!</h1>
+    </div>
+  )
+}
+
+export default App
